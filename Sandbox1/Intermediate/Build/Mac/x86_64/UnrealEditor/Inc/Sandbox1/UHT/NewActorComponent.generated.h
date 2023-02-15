0d0748c4e936ff1a556573ae6a87101d86db40e6
CMB1132 <122947785+CMB1132@users.noreply.github.com>
2023-02-14 20:56:20 -0500

    Added Unreal files

---
+++ Sandbox1/Intermediate/Build/Mac/x86_64/UnrealEditor/Inc/Sandbox1/UHT/NewActorComponent.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "NewActorComponent.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SANDBOX1_NewActorComponent_generated_h
+#error "NewActorComponent.generated.h already included, missing '#pragma once' in NewActorComponent.h"
+#endif
+#define SANDBOX1_NewActorComponent_generated_h
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_SPARSE_DATA
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_RPC_WRAPPERS
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_ACCESSORS
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUNewActorComponent(); \
+	friend struct Z_Construct_UClass_UNewActorComponent_Statics; \
+public: \
+	DECLARE_CLASS(UNewActorComponent, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sandbox1"), NO_API) \
+	DECLARE_SERIALIZER(UNewActorComponent)
+
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUNewActorComponent(); \
+	friend struct Z_Construct_UClass_UNewActorComponent_Statics; \
+public: \
+	DECLARE_CLASS(UNewActorComponent, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sandbox1"), NO_API) \
+	DECLARE_SERIALIZER(UNewActorComponent)
+
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UNewActorComponent(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UNewActorComponent) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UNewActorComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UNewActorComponent); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UNewActorComponent(UNewActorComponent&&); \
+	NO_API UNewActorComponent(const UNewActorComponent&); \
+public: \
+	NO_API virtual ~UNewActorComponent();
+
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UNewActorComponent(UNewActorComponent&&); \
+	NO_API UNewActorComponent(const UNewActorComponent&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UNewActorComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UNewActorComponent); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UNewActorComponent) \
+	NO_API virtual ~UNewActorComponent();
+
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_10_PROLOG
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_SPARSE_DATA \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_RPC_WRAPPERS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_ACCESSORS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_INCLASS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_SPARSE_DATA \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_ACCESSORS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_INCLASS_NO_PURE_DECLS \
+	FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SANDBOX1_API UClass* StaticClass<class UNewActorComponent>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_tui86819_Documents_Unreal_Projects_Sandbox1_Source_Sandbox1_NewActorComponent_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Sandbox1/Intermediate/Build/Mac/x86_64/UnrealEditor/Inc/Sandbox1/UHT/Sandbox1.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSandbox1_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Sandbox1;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Sandbox1()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Sandbox1.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Sandbox1",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x39980C02,
+				0xF09A9A55,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Sandbox1.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Sandbox1.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Sandbox1(Z_Construct_UPackage__Script_Sandbox1, TEXT("/Script/Sandbox1"), Z_Registration_Info_UPackage__Script_Sandbox1, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x39980C02, 0xF09A9A55));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
